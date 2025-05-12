c7a5fbaf42ff867a7d6086983882229f3224db4f
Reberte Martins <rebertemartins14@gmail.com>
2025-05-12 11:52:03 -0300

    feat(Lead) :sparkles:: Criando Factory para Lead

--- app/Models/Tenant/Lead.php
+++ app/Models/Tenant/Lead.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Tenant;
 
 use App\Models\Trait\TenantOwner;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Lead extends Model
 {
     //
-    use TenantOwner;
+    use HasFactory, TenantOwner;
 }
---
+++ database/factories/Tenant/LeadFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories\Tenant;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class LeadFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber()
+        ];
+    }
+}
