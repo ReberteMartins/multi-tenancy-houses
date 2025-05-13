8390416ec1ccabf56e53c929d2e8e650033051cd
Reberte Martins <rebertemartins14@gmail.com>
2025-05-13 10:26:14 -0300

    :sparkles:feat(Lead): Adiciona componente Livewire para gerenciamento de leads

---
+++ app/Livewire/LeadComponent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Tenant\Lead;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class LeadComponent extends Component
+{
+    use WithPagination;
+
+    public ?int $quantity = 5;
+
+    public ?string $search = null;
+
+    public array $headers = [
+                ['index' => 'id', 'label' => '#'],
+                ['index' => 'name', 'label' => 'Name'],
+                ['index' => 'email', 'label' => 'Email'],
+                ['index' => 'phone', 'label' => 'Phone'],
+                ['index' => 'created_at', 'label' => 'Created At']
+    ];
+
+    public function render(): View
+    {
+        return view('livewire.lead-component', [
+            'rows' => Lead::query()
+                ->when($this->search, function ($query, $search) {
+                    $this->search = trim($search);
+                    $query->where('name', 'like', "%{$this->search}%")
+                        ->orWhere('email', 'like', "%{$this->search}%")
+                        ->orWhere('phone', 'like', "%{$this->search}%");
+                })
+                ->paginate($this->quantity)
+                ->withQueryString(),
+        ]);
+    }
+}
---
+++ resources/views/livewire/lead-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <x-ts-table :$headers :$rows paginate filter :quantity="[5,10,20]"/>
+</div>
